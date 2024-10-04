083ac6f68dc4c1a515fd6a469a720dcd91a400b6
BenceR27 <rizb@kkszki.hu>
2024-10-04 11:38:53 +0200

    Beker is ready

---
+++ Penztar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penztar
+{
+    internal class Program
+    {
+        public static int[] tm = new int[] { 500, 1000, 2000, 5000, 10000, 20000 };
+        public static int[] money = new int[6];
+
+        public static void beker()
+        {
+            for (int i=0; i< tm.Length; i++ )
+            {
+                Console.WriteLine($"Kérem az {i+1} bankjegy értékét: ");
+                money[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            beker();
+        }
+    }
+}
